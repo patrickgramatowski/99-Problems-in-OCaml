1d2703e736b68f2e9b0e989340e97c4c1361c4b0
Patrick Gramatowski <63557278+patrickgramatowski@users.noreply.github.com>
2022-04-18 16:22:38 +0200

    Refactor solution for Problem 02

--- p02.ml
+++ p02.ml
@@ -1,5 +1,12 @@
-let rec last_two xs =
+let rec last_two (xs: 'a list): ('a * 'a) option =
   match xs with
-  | [] -> None
+  | [] -> None (* No match for [_] but it's still working as expected, why? -> '[] | [_] -> None' *)
   | [x;y] -> Some (x, y)
-  | h :: tl -> last_two tl
+  | _ :: tail -> last_two tail
+  
+  
+(* Refactored version - syntactic sugar -> https://www2.lib.uchicago.edu/keith/ocaml-class/pattern-matching.html *)
+let rec last_two_refactored = function
+    [] -> None
+  | [x;y] -> Some (x, y)
+  | _ :: tail -> last_two_refactored tail
